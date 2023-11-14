bdc5273687e4b7e6b96a921fb69501007af03ca9
karko13 <145532713+karko13@users.noreply.github.com>
2023-11-14 18:45:37 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PripremaVjezba05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upisi te neki string");
+            string x = Console.ReadLine();
+            int y = x.Length;
+            if(y < 5)
+                {
+                Console.WriteLine(x);
+                }
+            Console.WriteLine(x.Substring(2, y - 4));
+            Console.ReadKey();
+        }
+    }
+}
